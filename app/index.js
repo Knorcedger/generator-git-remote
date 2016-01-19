9192c14068aec70affa10dc229105c512e2deee9
Achilleas Tsoumitas <knorcedger@gmail.com>
2016-01-19 17:59:32 +0200

    Added check if the folder is git repo

--- app/index.js
+++ app/index.js
@@ -3,19 +3,33 @@ var generators = require('yeoman-generator');
 
 module.exports = generators.Base.extend({
 	prompting: function() {
-		var done = this.async();
-		var self = this;
-		this.prompt({
-			type: 'input',
-			name: 'remoteUrl',
-			message: 'What is the git remote url (origin)?'
-		}, function(answers) {
-			if (answers.remoteUrl) {
-				self.spawnCommandSync('git', ['remote', 'add', 'origin',
+		// check if the current directory is git
+		// if not, don't crash, just log a message
+		// we check if isGit by trying to read the config file in .git folder
+		var isGit;
+		try {
+			isGit = Boolean(this.read(this.destinationRoot() + '/.git/config'));
+		} catch (e) {
+
+		}
+		if (isGit) {
+			var done = this.async();
+			var self = this;
+			this.prompt({
+				type: 'input',
+				name: 'remoteUrl',
+				message: 'What is the git remote url (origin)?'
+			}, function(answers) {
+				if (answers.remoteUrl) {
+					self.spawnCommandSync('git', ['remote', 'add', 'origin',
 					answers.remoteUrl]);
-				console.log('Git remote added!');
-			}
-			done();
-		});
+					console.log('Git remote added!');
+				}
+				done();
+			});
+		} else {
+			console.log('Not a git repo, can\'t add a remote.');
+			console.log('Please "git init" first.');
+		}
 	}
 });
